44a9627fa421d1d88ffd42aa072dd617467e220c
Ibnu-Alinsani <126755759+Ibnu-Alinsani@users.noreply.github.com>
2023-06-25 19:30:22 +0700

    fix income book

--- src/pages/detail-book/index.js
+++ src/pages/detail-book/index.js
@@ -1,183 +1,321 @@
-import { Col, Container, Row, Button } from "react-bootstrap"
-import { useMutation, useQuery } from "react-query"
-import * as UTILS from "../../utils"
-import * as IMG from "../../assets"
-import { API } from "../../config/api"
-import { FallingLines } from  'react-loader-spinner'
-import { useNavigate, useParams } from "react-router-dom"
+import { Col, Container, Row, Button } from "react-bootstrap";
+import { useMutation, useQuery } from "react-query";
+import * as UTILS from "../../utils";
+import * as IMG from "../../assets";
+import { API } from "../../config/api";
+import { FallingLines } from "react-loader-spinner";
+import { useNavigate, useParams } from "react-router-dom";
 import Swal from "sweetalert2";
-import {useEffect, useContext} from "react"
-import { UserContext } from "../../context"
+import { useEffect, useContext } from "react";
+import { UserContext } from "../../context";
 
+function DetailBook() {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
 
-function DetailBook()  {
-    useEffect(() => {
-        window.scrollTo(0, 0)
-    }, [])
+  document.title = "Detail Book";
 
-    document.title = "Detail Book"
+  const [state, _] = useContext(UserContext);
+  console.log(state);
+  const { id } = useParams();
 
-    const [state, _] = useContext(UserContext)
-    console.log(state)
-    const { id } = useParams()
+  const { data: book, isLoading } = useQuery("bookCache", async () => {
+    const response = await API.get(`/book/${id}`);
+    return response.data.data;
+  });
+  // console.log(book.id);
 
-    const { data: book, isLoading } = useQuery("bookCache", async () => {
-        const response = await API.get(`/book/${id}`)
-        return response.data.data
-    })
+  const navigate = useNavigate();
 
-    const navigate = useNavigate()
+  const AddCart = useMutation(async (e) => {
+    try {
+      if (!state.IsLogin) {
+        return Swal.fire({
+          title: "Warning",
+          text: "Please login to be proceed",
+          icon: "warning",
+          showConfirmButton: false,
+          timer: 2000,
+        });
+      }
 
-    const AddCart = useMutation(async (e) => {
-        try {
+      const response = await API.post("/cart", {
+        book_id: e,
+      });
 
-            if (!state.IsLogin) {
-                return Swal.fire({
-                    title: "Warning",
-                    text: "Please login to be proceed",
-                    icon: "warning",
-                    showConfirmButton: false,
-                    timer: 2000,
-                  });
-            }
-
-            const response = await API.post("/cart", {
-                book_id: e
-            })
-            
-            Swal.fire({
-                title: "Add Success",
-                icon: "success",
-                showConfirmButton: false,
-                timer: 2000,
-            });
-
-        } catch (error) {
-            Swal.fire({
-                title: "Add Failed",
-                text: `${error.response.data.message}`,
-                icon: "warning",
-                showConfirmButton: false,
-                timer: 2000,
-            });
-        }
-    })
-
-    const deleteBook = useMutation(async (e) => {
-        try {
-
-            if (book.transaction.length > 0) {
-                return Swal.fire({
-                    title: "Warning",
-                    text: "This book have transaction",
-                    icon: "warning",
-                    showConfirmButton: false,
-                    timer: 2000,
-                  });
-            }
-
-            const response = API.delete(`/book/${e}`)
-        } catch (error) {
-            Swal.fire({
-                title: "Delete Failed",
-                text: `Please try again later`,
-                icon: "warning",
-                showConfirmButton: false,
-                timer: 2000,
-            });
-            console.log("delete failed", error)
-        }
-    })
-
-    //Download 
-    const downloadPdf = (url) => {
-        fetch(url).then(response => response.blob()).then(blob => {
-            const blobURL = window.URL.createObjectURL(new Blob([blob]));
-            const fileName = url.split("/").pop();
-            const a = document.createElement("a");
-            a.href = blobURL;
-            a.download = fileName;
-            document.body.appendChild(a);
-            a.click();
-            a.remove();
-        })
+      Swal.fire({
+        title: "Add Success",
+        icon: "success",
+        showConfirmButton: false,
+        timer: 2000,
+      });
+    } catch (error) {
+      Swal.fire({
+        title: "Add Failed",
+        text: `${error.response.data.message}`,
+        icon: "warning",
+        showConfirmButton: false,
+        timer: 2000,
+      });
     }
+  });
 
-    return isLoading ? 
-    (   
-        <Container fluid className=" text-center d-flex justify-content-center align-items-center" style={{height: "55vh"}}>
-            <FallingLines
-                color="#242424"
-                width="100"
-                visible={true}
-                className="m-auto"
-                ariaLabel='falling-lines-loading'
-            /> 
-        </Container>
-    ) : 
-    (
-        <Container className="mt-5 text-wrap position-relative" style={{marginBottom:"10rem", maxWidth:"1000px"}}>
-            <Row>
-                <Col>
-                    <img src={book.thumbnail} alt="..." width="400px" height="577px" style={{borderRadius:"10px"}} className="object-fit-cover"/>
-                </Col>
-                <Col>
-                    <div className="d-flex flex-column gap-3 justify-content-between h-100">
-                        <div>
-                            <p className="fw-bold" style={{fontFamily:"Times New Roman", fontSize:"48px", marginBottom:"0", lineHeight:"60px"}}>{book.title}</p>
-                            <i className="text-muted d-block mt-2" style={{fontSize:"24px"}}>By. {book.author}</i>
-                        </div>
-                        <div className="mt-3">
-                            <b style={{marginBottom:"0", fontSize:"24px"}}>Publication date</b>
-                            <p className="text-muted mt-2" style={{fontSize:"18px"}}>{book.publication_date}</p>
-                        </div>
-                        <div className="mt-3">
-                            <b style={{marginBottom:"0", fontSize:"24px"}}>Pages</b>
-                            <p className="text-muted mt-2" style={{fontSize:"18px"}}>{book.pages}</p>
-                        </div>
-                        <div>
-                            <b className="text-danger" style={{marginBottom:"0", fontSize:"24px"}}>ISBN</b>
-                            <p className="text-muted mt-2" style={{fontSize:"18px"}}>{book.isbn}</p>
-                        </div>
-                        <div>
-                            <b style={{marginBottom:"0", fontSize:"24px"}}>Price</b>
-                            <p className="text-success mt-2" style={{fontSize:"18px"}}>
-                                {new Intl.NumberFormat("id-ID", {
-                                    style: "currency",
-                                    currency: "IDR",
-                                }).format(book.price)}
-                            </p>
-                        </div>
-                    </div>
-                </Col>
-            </Row>
-            <Col className="mt-5">
-                <h1 style={{fontFamily:"Times New Roman", fontSize:"36px", marginBottom:"1.3rem"}}>About This Book</h1>
-                <UTILS.AutoParagraph text={book.description} wordsPerParagraph={60}/>
-            </Col>
-            {book && (
-                <>
-                    {state.user.role === "user" ? book.transaction.filter(item => item.user_id === state.user.id).length > 0 ? (
-                    <Button variant="dark" className="mt-5 rounded-0 position-absolute end-0 left-0 px-4" onClick={() => downloadPdf(book.book_attachment)}>
-                        Download
-                    </Button>
-                    ) : (
-                    <Button variant="dark" className="mt-5 rounded-0 position-absolute end-0 left-0" onClick={(e) => AddCart.mutate(book.id)}>
-                        Add Cart
-                        <img src={IMG.Cart2} alt="..." width="24" height="22" className="ms-2"/>
-                    </Button>
-                    ) : (<div>
-                        <Button variant="dark" className="mt-5 px-5 rounded-0 position-absolute end-0" onClick={() => navigate(`/update-book/${book.id}`)}>
-                            Update
-                        </Button>
-                        <Button variant="danger" className="mt-5 px-5 rounded-0 position-absolute" style={{right:"20%"}} onClick={() => deleteBook.mutate(book.id)}>
-                            Delete
-                        </Button>
-                    </div>)}
-                </>
-            )}
-        </Container>
-    );
+  const deleteBook = useMutation(async (e) => {
+    try {
+      if (book.transaction.length > 0) {
+        Swal.fire({
+          title: "Warning",
+          text: "This book have transaction",
+          icon: "warning",
+          showConfirmButton: false,
+          timer: 2000,
+        });
+        return;
+      }
+
+      const response = API.delete(`/book/${e}`);
+      console.log("delete success", response.data.data);
+    } catch (error) {
+      Swal.fire({
+        title: "Delete Failed",
+        text: `Please try again later`,
+        icon: "warning",
+        showConfirmButton: false,
+        timer: 2000,
+      });
+      console.log("delete failed", error);
+    }
+  });
+
+  //Download
+  const downloadPdf = (url) => {
+    fetch(url)
+      .then((response) => response.blob())
+      .then((blob) => {
+        const blobURL = window.URL.createObjectURL(new Blob([blob]));
+        const fileName = url.split("/").pop();
+        const a = document.createElement("a");
+        a.href = blobURL;
+        a.download = fileName;
+        document.body.appendChild(a);
+        a.click();
+        a.remove();
+      });
+  };
+
+  return isLoading ? (
+    <Container
+      fluid
+      className=" text-center d-flex justify-content-center align-items-center"
+      style={{ height: "55vh" }}
+    >
+      <FallingLines
+        color="#242424"
+        width="100"
+        visible={true}
+        className="m-auto"
+        ariaLabel="falling-lines-loading"
+      />
+    </Container>
+  ) : (
+    <Container
+      className="mt-5 text-wrap position-relative"
+      style={{ marginBottom: "10rem", maxWidth: "1000px" }}
+    >
+      <Row>
+        <Col>
+          <img
+            src={book?.thumbnail}
+            alt="..."
+            width="400px"
+            height="577px"
+            style={{ borderRadius: "10px" }}
+            className="object-fit-cover img-thumb"
+          />
+        </Col>
+        <Col>
+          <div className="d-flex flex-column gap-3 justify-content-between h-100">
+            <div>
+              <p
+                className="fw-bold"
+                style={{
+                  fontFamily: "Times New Roman",
+                  fontSize: "48px",
+                  marginBottom: "0",
+                  lineHeight: "60px",
+                }}
+              >
+                {book?.title}
+              </p>
+              <i
+                className="text-muted d-block mt-2"
+                style={{ fontSize: "24px" }}
+              >
+                By. {book?.author}
+              </i>
+            </div>
+            <div className="mt-3">
+              <b style={{ marginBottom: "0", fontSize: "24px" }}>
+                Publication date
+              </b>
+              <p className="text-muted mt-2" style={{ fontSize: "18px" }}>
+                {book?.publication_date}
+              </p>
+            </div>
+            <div className="mt-3">
+              <b style={{ marginBottom: "0", fontSize: "24px" }}>Pages</b>
+              <p className="text-muted mt-2" style={{ fontSize: "18px" }}>
+                {book?.pages}
+              </p>
+            </div>
+            <div>
+              <b
+                className="text-danger"
+                style={{ marginBottom: "0", fontSize: "24px" }}
+              >
+                ISBN
+              </b>
+              <p className="text-muted mt-2" style={{ fontSize: "18px" }}>
+                {book?.isbn}
+              </p>
+            </div>
+            <div>
+              <b style={{ marginBottom: "0", fontSize: "24px" }}>Price</b>
+              <p className="text-success mt-2" style={{ fontSize: "18px" }}>
+                {new Intl.NumberFormat("id-ID", {
+                  style: "currency",
+                  currency: "IDR",
+                }).format(book?.price)}
+              </p>
+            </div>
+          </div>
+        </Col>
+      </Row>
+      <Col className="mt-5">
+        <h1
+          style={{
+            fontFamily: "Times New Roman",
+            fontSize: "36px",
+            marginBottom: "1.3rem",
+          }}
+        >
+          About This Book
+        </h1>
+        <UTILS.AutoParagraph text={book?.description} wordsPerParagraph={60} />
+      </Col>
+
+      {book && (
+        <>
+          {state.user.role === "user" ? (
+            book.transaction.filter((item) => item.user_id === state.user.id)
+              .length > 0 ? (
+              <Button
+                variant="dark"
+                className="mt-5 rounded-0 position-absolute end-0 left-0 px-4"
+                onClick={() => downloadPdf(book.book_attachment)}
+              >
+                Download
+              </Button>
+            ) : (
+              <Button
+                variant="dark"
+                className="mt-5 rounded-0 position-absolute end-0 left-0"
+                onClick={(e) => AddCart.mutate(book.id)}
+              >
+                Add Cart
+                <img
+                  src={IMG.Cart2}
+                  alt="..."
+                  width="24"
+                  height="22"
+                  className="ms-2"
+                />
+              </Button>
+            )
+          ) : state.user.role === "admin" ? (
+            <div>
+              <Button
+                variant="dark"
+                className="mt-5 px-5 rounded-0 position-absolute end-0"
+                onClick={() => navigate(`/update-book/${book.id}`)}
+              >
+                Update
+              </Button>
+              <Button
+                variant="danger"
+                className="mt-5 px-5 rounded-0 position-absolute"
+                style={{ right: "20%" }}
+                onClick={() => deleteBook.mutate(book.id)}
+              >
+                Delete
+              </Button>
+            </div>
+          ) : (
+            <Button
+              variant="dark"
+              className="mt-5 rounded-0 position-absolute end-0 left-0"
+              onClick={(e) => AddCart.mutate(book.id)}
+            >
+              Add Cart
+              <img
+                src={IMG.Cart2}
+                alt="..."
+                width="24"
+                height="22"
+                className="ms-2"
+              />
+            </Button>
+          )}
+
+          {/* {state.user.role === "user" ? (
+            book.transaction.filter((item) => item.user_id === state.user.id)
+            .length > 0 ? (
+            <Button
+              variant="dark"
+              className="mt-5 rounded-0 position-absolute end-0 left-0 px-4"
+              onClick={() => downloadPdf(book.book_attachment)}
+            >
+              Download
+            </Button>
+          ) : state.user.role === "admin" ? (
+            <Button
+                variant="dark"
+                className="mt-5 rounded-0 position-absolute end-0 left-0"
+                onClick={(e) => AddCart.mutate(book.id)}
+              >
+                Add Cart
+                <img
+                  src={IMG.Cart2}
+                  alt="..."
+                  width="24"
+                  height="22"
+                  className="ms-2"
+                />
+            </Button>
+          ) : (
+            <div>
+              <Button
+                variant="dark"
+                className="mt-5 px-5 rounded-0 position-absolute end-0"
+                onClick={() => navigate(`/update-book/${book.id}`)}
+              >
+                Update
+              </Button>
+              <Button
+                variant="danger"
+                className="mt-5 px-5 rounded-0 position-absolute"
+                style={{ right: "20%" }}
+                onClick={() => deleteBook.mutate(book.id)}
+              >
+                Delete
+              </Button>
+            </div>
+          )) :} */}
+        </>
+      )}
+    </Container>
+  );
 }
 
-export default DetailBook
+export default DetailBook;
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,6 +1,6 @@
 import { Container, Button } from "react-bootstrap";
-import * as react from "react"
-import * as IMG from "../../assets"
+import * as react from "react";
+import * as IMG from "../../assets";
 import * as COMP from "../../component";
 import { UserContext } from "../../context";
 import { API } from "../../config/api";
@@ -10,39 +10,41 @@ import { FallingLines } from "react-loader-spinner";
 import { Link } from "react-router-dom";
 
 function Home() {
-    react.useEffect(() => {
-      window.scrollTo(0, 0)
-    }, [])
-    
-    document.title = "Home"
+  react.useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
 
-    const [state, dispatch] = react.useContext(UserContext)
-    
-    // Top 3
-    const {data : book, isLoading} = useQuery("populerCache", async () => {
-      const response = await API.get("/books")
-      return response.data.data
-    })
+  document.title = "Home";
 
-    const [topThree, setTopThree] = react.useState([])
-    react.useEffect(() => {
-      const sorted = book?.sort((a,b) => b.transaction.length - a.transaction.length)
-      setTopThree(sorted?.slice(0, 3))
-    }, [book])
+  const [state, dispatch] = react.useContext(UserContext);
 
-    react.useEffect(() => {
-        if (state.user.name) {
-            const intervalId = setInterval(() => {
-                document.title = `Halo, ${state.user.name}` 
-            }, 1000);
+  // Top 3
+  const { data: book, isLoading } = useQuery("populerCache", async () => {
+    const response = await API.get("/books");
+    return response.data.data;
+  });
 
-            setTimeout(() => {
-                clearInterval(intervalId);
-                document.title = `Home` 
-            }, 5000)
-        }
-    }, [state])
-    
+  const [topThree, setTopThree] = react.useState([]);
+  react.useEffect(() => {
+    const sorted = book?.sort(
+      (a, b) => b.transaction.length - a.transaction.length
+    );
+    setTopThree(sorted?.slice(0, 3));
+  }, [book]);
+
+  react.useEffect(() => {
+    if (state.user.name) {
+      const intervalId = setInterval(() => {
+        document.title = `Halo, ${state.user.name}`;
+      }, 1000);
+
+      setTimeout(() => {
+        clearInterval(intervalId);
+        document.title = `Home`;
+      }, 5000);
+    }
+  }, [state]);
+
   // Drag Event
   const containerRef = react.useRef(null);
   let isDragging = false;
@@ -89,55 +91,59 @@ function Home() {
 
   const AddCart = useMutation(async (e) => {
     try {
-      
-        if (!state.IsLogin) {
-          return Swal.fire({
-            title: "Warning",
-            text: "Please login to be proceed",
-            icon: "warning",
-            showConfirmButton: false,
-            timer: 2000,
-          });
-        }
-
-        const response = await API.post("/cart", {
-            book_id: e
-        })
-      
-        Swal.fire({
-            title: "Add Success",
-            text: `Added Book`,
-            icon: "success",
-            showConfirmButton: false,
-            timer: 2000,
+      if (!state.IsLogin) {
+        return Swal.fire({
+          title: "Warning",
+          text: "Please login to be proceed",
+          icon: "warning",
+          showConfirmButton: false,
+          timer: 2000,
         });
+      }
 
+      const response = await API.post("/cart", {
+        book_id: e,
+      });
+
+      Swal.fire({
+        title: "Add Success",
+        text: `Added Book`,
+        icon: "success",
+        showConfirmButton: false,
+        timer: 2000,
+      });
     } catch (error) {
-        Swal.fire({
-            title: "Add Failed",
-            text: `${error.response.data.message}`,
-            // text: `error mase`,
-            icon: "warning",
-            showConfirmButton: false,
-            timer: 2000,
-        });
+      Swal.fire({
+        title: "Add Failed",
+        text: `${error.response.data.message}`,
+        // text: `error mase`,
+        icon: "warning",
+        showConfirmButton: false,
+        timer: 2000,
+      });
     }
-})
+  });
 
-  return isLoading ? 
-  (
-    <Container fluid className=" text-center d-flex justify-content-center align-items-center" style={{height: "55vh"}}>
-        <FallingLines
-            color="#242424"
-            width="100"
-            visible={true}
-            className="m-auto"
-            ariaLabel='falling-lines-loading'
-        /> 
+  return isLoading ? (
+    <Container
+      fluid
+      className=" text-center d-flex justify-content-center align-items-center"
+      style={{ height: "55vh" }}
+    >
+      <FallingLines
+        color="#242424"
+        width="100"
+        visible={true}
+        className="m-auto"
+        ariaLabel="falling-lines-loading"
+      />
     </Container>
   ) : (
     <Container fluid className="mt-5 px-0">
-      <h1 className="text-center" style={{ fontFamily: "Times New Roman", fontSize: "48px" }}>
+      <h1
+        className="text-center"
+        style={{ fontFamily: "Times New Roman", fontSize: "48px" }}
+      >
         With us, you can shop online & help
         <br />
         save your high street at the same time
@@ -146,42 +152,91 @@ function Home() {
         ref={containerRef}
         fluid
         className="px-0 d-flex overflow-x-hidden"
-        style={{ gap: "3.3rem", marginTop:"6rem"}}
+        style={{ gap: "3.3rem", marginTop: "6rem" }}
       >
-        {
-          topThree && topThree.map((book, index) => (
-            <Link to={`/detail-book/${book.id}`} key={index}className="text-decoration-none text-dark" >
-              <div className="d-flex align-items-center position-relative" draggable="true" onDragStart={dragStart}>
-                <img src={IMG.Best} alt="best" width="60px" height="60px" className="object-fit-cover position-absolute top-0 end-50"/>
-                <img src={book.thumbnail} alt="..." width="236px" height="345px" className="object-fit-cover"/>
-                <div className="p-3 pb-3" style={{ width: "268px", height: "277px" }}>
-                  <b style={{ fontFamily: "Times New Roman", fontSize: "24px", lineHeight: "15px" }}>
+        {topThree &&
+          topThree.map((book, index) => (
+            <Link
+              to={`/detail-book/${book.id}`}
+              key={index}
+              className="text-decoration-none text-dark"
+            >
+              <div
+                className="d-flex align-items-center position-relative"
+                draggable="true"
+                onDragStart={dragStart}
+              >
+                <img
+                  src={IMG.Best}
+                  alt="best"
+                  width="60px"
+                  height="60px"
+                  className="object-fit-cover position-absolute top-0 end-50"
+                />
+                <img
+                  src={book.thumbnail}
+                  alt="..."
+                  width="236px"
+                  height="345px"
+                  className="object-fit-cover"
+                />
+                <div
+                  className="p-3 pb-3"
+                  style={{ width: "268px", height: "277px" }}
+                >
+                  <b
+                    style={{
+                      fontFamily: "Times New Roman",
+                      fontSize: "24px",
+                      lineHeight: "15px",
+                    }}
+                  >
                     {book.title}
                   </b>
                   <small className="d-block mb-3 text-muted">
                     <i>By. {book.author}</i>
                   </small>
-                  <p className="mb-4" style={{ lineHeight: "121,5%", fontSize: "14px" }}>
+                  <p
+                    className="mb-4"
+                    style={{ lineHeight: "121,5%", fontSize: "14px" }}
+                  >
                     {book.description.substr(0, 16)} ...
                   </p>
-                  <p className="mb-2 text-success fw-bold" style={{ fontSize: "18px" }}>
+                  <p
+                    className="mb-2 text-success fw-bold"
+                    style={{ fontSize: "18px" }}
+                  >
                     {new Intl.NumberFormat("id-ID", {
-                        style: "currency",
-                        currency: "IDR",
+                      style: "currency",
+                      currency: "IDR",
                     }).format(book.price)}
                   </p>
-                  <Button variant="dark" className="rounded-0 w-100" onClick={(e) => AddCart.mutate(book.id)}>
-                    Add To Cart
-                  </Button>
+                  {book.transaction.filter((e) => e.user_id === state.user.id)
+                    .length > 0 ? (
+                    <Button
+                      variant="dark"
+                      className="rounded-0 w-100"
+                      onClick={(e) => AddCart.mutate(book.id)}
+                    >
+                      Download
+                    </Button>
+                  ) : (
+                    <Button
+                      variant="dark"
+                      className="rounded-0 w-100"
+                      onClick={(e) => AddCart.mutate(book.id)}
+                    >
+                      Add To Cart
+                    </Button>
+                  )}
                 </div>
               </div>
             </Link>
-          ))
-        }
+          ))}
       </Container>
 
-      <div className="px-0 mx-auto" style={{width:"1200px"}}>
-          <COMP.BookList />
+      <div className="px-0 mx-auto" style={{ width: "1200px" }}>
+        <COMP.BookList />
       </div>
     </Container>
   );
